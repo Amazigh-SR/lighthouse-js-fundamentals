82885a2bdcd2faa0057302e7c565d24f00d252a4
Saïd Redouane <s_red009@hotmail.com>
2020-12-09 06:36:27 +0000

    Even more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -26,3 +26,25 @@
 
 // ======================================== \\
 
+// const isCitizen = true;
+// const age = 26;
+
+// if (isCitizen && age > 18) {
+//   console.log("You are eligible to vote.")
+// }
+
+// ======================================== \\
+
+
+
+const temperature = 25;
+const raining = true;
+
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea...");
+}
+
+if(!raining) {
+  console.log("Leave your umbreall at home!");
+}
