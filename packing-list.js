e52d7fd7d59044cb070918e525c5f4bbc4ff2d52
Saïd Redouane <s_red009@hotmail.com>
2020-12-12 06:25:09 +0000

    Transformed the existing for loop into a while loop

--- packing-list.js
+++ packing-list.js
@@ -1,7 +1,17 @@
 //List of kitchen items
+
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
+//For loop
 console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 }
+
+//while loop
+let i = 0;
+while(i < packingList.length) {
+  console.log(packingList[i])
+  i++;
+}
+
